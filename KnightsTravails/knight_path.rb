063980c33174efbbcf30d2d6b0341af9969d9759
AA Student <appacademy@Mini-43.local>
2022-03-11 17:01:38 -0800

    Finish knight pathfinder

--- KnightsTravails/knight_path.rb
+++ KnightsTravails/knight_path.rb
@@ -4,6 +4,7 @@ require_relative '../TreeNode/lib/00_tree_node'
 class KnightPathFinder
     
     def self.valid_moves(positions, cons_pos)
+        # Refactor this to filter out of considered positions in new_move_positions
         result = []
         positions.each do |pos|
             x, y = pos
@@ -24,6 +25,7 @@ class KnightPathFinder
                 @possible_moves << [i,j] << [j,i]
             end
         end     
+        build_move_tree
     end
 
     def new_move_positions(node)
@@ -48,4 +50,21 @@ class KnightPathFinder
         return @origin
     end
 
+    def find_path(end_pos)
+        target = @origin.dfs(end_pos)
+        return trace_path_back(target)
+    end
+
+    def trace_path_back(leaf) 
+        result = []
+        while leaf != nil
+            result.unshift(leaf.value)
+            leaf = leaf.parent
+        end
+        result
+    end
 end
+
+kpf = KnightPathFinder.new([0, 0])
+p kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+p kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
